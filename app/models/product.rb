a28e8f2ab3350ba29140e6dd42808bd3c9fc96d8
韓豪 <kangochan515@gmail.com>
2016-09-20 19:59:20 +0900

    add like function

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,16 @@
 class Product < ActiveRecord::Base
   has_many :product_likes, dependent: :destroy
-  def like_user(user_id)
-    likes.find_by(user_id: user_id)
-  end
   has_many :product_structures
   has_many :products_casts
   has_many :casts, through: :products_casts
   has_many :reviews
+
+  def product_like_user(user_id)
+    product_likes.find_by(user_id: user_id)
+  end
+
+  def review_like_user(user_id)
+    review_likes.find_by(user_id: user_id)
+  end
+
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,11 @@
 class Review < ActiveRecord::Base
   belongs_to :product
   belongs_to :user
-  has_many :review_likes
+  has_many :review_likes, dependent: :destroy
   has_many :review_comments
+
+  def review_like_user(user_id)
+    review_likes.find_by(user_id: user_id)
+  end
+  
 end
